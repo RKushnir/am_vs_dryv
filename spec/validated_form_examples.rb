f1a7842c33ff81df6994e52ff1e728007aaf0137
Roman Kushnir <roman.kushnir@ad2games.com>
2016-09-02 20:38:25 +0200

    Refactor specs

--- spec/validated_form_examples.rb
+++ spec/validated_form_examples.rb
@@ -1,37 +1,37 @@
 RSpec.shared_examples 'a validated form' do
-  def expect_success(*args)
-    expect(described_class.new(*args)).to be_valid
+  def expect_success(start_date: '2016-01-02', end_date: '2016-02-03')
+    expect(described_class.new(start_date, end_date)).to be_valid
   end
 
-  def expect_failure(*args)
-    expect(described_class.new(*args)).not_to be_valid
+  def expect_failure(start_date: '2016-01-02', end_date: '2016-02-03')
+    expect(described_class.new(start_date, end_date)).not_to be_valid
   end
 
   it 'passes valid values' do
-    expect_success '2016-01-02', '2016-02-03'
+    expect_success start_date: '2016-01-02', end_date: '2016-02-03'
   end
 
   it 'validates start_date is present' do
-    expect_failure nil, '2016-02-03'
-    expect_failure '', '2016-02-03'
+    expect_failure start_date: nil
+    expect_failure start_date: ''
   end
 
   it 'validates start_date is a valid date' do
-    expect_failure 'wrong', '2016-02-03'
-    expect_failure '2016-50-10', '2016-02-03'
+    expect_failure start_date: 'wrong'
+    expect_failure start_date: '2016-50-10'
   end
 
   it 'validates end_date is present' do
-    expect_failure '2016-01-02', nil
-    expect_failure '2016-01-02', ''
+    expect_failure end_date: nil
+    expect_failure end_date: ''
   end
 
   it 'validates end_date is a valid date' do
-    expect_failure '2016-01-02', 'wrong'
-    expect_failure '2016-01-02', '2016-50-10'
+    expect_failure end_date: 'wrong'
+    expect_failure end_date: '2016-50-10'
   end
 
   it 'validates start_date is before the end_date' do
-    expect_failure '2016-02-03', '2016-01-02'
+    expect_failure start_date: '2016-02-03', end_date: '2016-01-02'
   end
 end
